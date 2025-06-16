fc085c4c20c514f56fd05f60a7d3fac4655023a9
Saurabh Damde <saurabhdamde@gmail.com>
2025-06-17 00:01:50 +0530

    Return sum of two comma-separated numbers

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ export const add = (numStr: string): number => {
   if (!numStr || numStr.trim() === "") {
     return 0;
   }
-  return parseInt(numStr);
+  const numbers = numStr.split(",").map(Number);
+  const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+  return sum;
 };
--- src/tests/calculator.test.ts
+++ src/tests/calculator.test.ts
@@ -4,4 +4,6 @@ describe("String Calculator", () => {
   test("Return 0 for empty string", () => expect(add("")).toBe(0));
   test("Return number itself for single number", () =>
     expect(add("5")).toBe(5));
+  test("Return sum of two comma-separated numbers", () =>
+    expect(add("2,3")).toBe(5));
 });
